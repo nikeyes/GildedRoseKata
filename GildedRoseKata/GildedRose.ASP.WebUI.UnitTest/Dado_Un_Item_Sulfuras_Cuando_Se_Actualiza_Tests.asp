20e82c9ad09b05c6ef1e73fae1d76f2c8922f61e
jorge.castro <nikey_es@yahoo.es>
2014-08-03 23:53:27 +0200

    Añadido Test Sulfuras Sellin

--- GildedRoseKata/GildedRose.ASP.WebUI.UnitTest/Dado_Un_Item_Sulfuras_Cuando_Se_Actualiza_Tests.asp
+++ GildedRoseKata/GildedRose.ASP.WebUI.UnitTest/Dado_Un_Item_Sulfuras_Cuando_Se_Actualiza_Tests.asp
@@ -2,7 +2,7 @@
 Class Dado_Un_Item_Sulfuras_Cuando_Se_Actualiza_Tests
 
 	Public Function TestCaseNames()
-		TestCaseNames = Array("Entonces_Calidad_Nunca_Disminuye")
+		TestCaseNames = Array("Entonces_Calidad_Nunca_Disminuye, Entonces_Sellin_No_Disminuye")
 	End Function
 
 	Public Sub SetUp()
@@ -34,6 +34,29 @@ Class Dado_Un_Item_Sulfuras_Cuando_Se_Actualiza_Tests
 		'ASSERT
 		
 		oTestResult.AssertEquals expectedQuality, actualQuality, "Error Item Sulfuras Disminuye Calidad"
+	End Sub
+
+    Public Sub Entonces_Sellin_No_Disminuye(oTestResult)
+		'ARRANGE
+		Dim expectedSellin 
+		expectedSellin = 1
+
+        Dim sut
+        Set sut = New Program
+       Set objItem = New Item
+        objItem.Name = "Sulfuras, Hand of Ragnaros"
+        objItem.Sellin = 1
+        objItem.Quality = 80
+        sut.Items.Add sut.Items.Count, objItem       
+
+		'ACT
+		Dim actualSellin
+        sut.UpdateQuality()
+        actualSellin = sut.Items(0).Sellin
+
+		'ASSERT
+		
+		oTestResult.AssertEquals expectedSellin, actualSellin, "Error Item Standard Sellin Disminuye"
 	End Sub
 
     
--- GildedRoseKata/GildedRose.ASP.WebUI.UnitTest/TestEngine/TestRunner.cs
+++ GildedRoseKata/GildedRose.ASP.WebUI.UnitTest/TestEngine/TestRunner.cs
@@ -445,6 +445,44 @@
 	}
 
 	[TestMethod]
+	[TestCategory("Dado_Un_Item_Sulfuras_Cuando_Se_Actualiza_Tests")]
+	public void Dado_Un_Item_Sulfuras_Cuando_Se_Actualiza_Tests_Entonces_Sellin_No_DisminuyeTest()
+	{
+		StringBuilder urlAspUnitTest = new StringBuilder(); 
+		urlAspUnitTest.Append(BASE_URL);
+		urlAspUnitTest.Append("&cboTestContainers=");
+		urlAspUnitTest.Append("Dado_Un_Item_Sulfuras_Cuando_Se_Actualiza_Tests");            
+		urlAspUnitTest.Append("&cboTestCases=");
+		urlAspUnitTest.Append("Entonces_Sellin_No_Disminuye");
+		urlAspUnitTest.Append("&chkShowSuccess=on");
+		urlAspUnitTest.Append("&cmdRun=Run%20Tests");
+
+
+		WebRequest request = WebRequest.Create(urlAspUnitTest.ToString());
+
+		ICredentials requestCredentials = CredentialCache.DefaultCredentials;
+		request.Credentials = requestCredentials;
+
+		using (WebResponse response = request.GetResponse())
+		{
+			using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+			{
+				string json = stream.ReadToEnd();
+
+				List<TestResult> results = JsonConvert.DeserializeObject<List<TestResult>>(json);;
+
+				foreach (TestResult result in results)
+				{
+					Assert.IsTrue(result.ResultType == "Success", result.Description);
+				}
+				//Assert.IsFalse(html.Contains("Failure"));
+				//Assert.IsFalse(html.Contains("Error"));
+			}
+		}
+
+	}
+
+	[TestMethod]
 	[TestCategory("TEMPLATE_AllFail_Tests")]
 	public void TEMPLATE_AllFail_Tests_TestAssertsTypesTest()
 	{

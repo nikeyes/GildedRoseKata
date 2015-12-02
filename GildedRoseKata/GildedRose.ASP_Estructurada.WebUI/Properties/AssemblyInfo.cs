483444999476d21f088fa8cf9c4f1c5f7bd6c1ac
Jorge Castro <nikey_es@yahoo.es>
2015-12-02 20:58:47 +0100

    Separar proyectos Classic ASP en Programación estructurada y OOP

--- GildedRoseKata/GildedRose.ASP_Estructurada.WebUI/Properties/AssemblyInfo.cs
+++ GildedRoseKata/GildedRose.ASP_Estructurada.WebUI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GildedRose.ASP")]
+[assembly: AssemblyTitle("GildedRose.ASP_Estructurado.WebUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GildedRose.ASP")]
+[assembly: AssemblyCompany("nikeyes")]
+[assembly: AssemblyProduct("GildedRose.ASP_Estructurado.WebUI")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

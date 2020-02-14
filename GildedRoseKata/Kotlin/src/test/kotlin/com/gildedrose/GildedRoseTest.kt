599610906b0e7d555182301bcfd8964a61695023
FUSION\jorge.castro <jorge.castro@habitaclia.com>
2020-02-14 08:10:46 +0100

    Del kotlin test

--- GildedRoseKata/Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ GildedRoseKata/Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -6,10 +6,7 @@ import org.junit.Test
 class GildedRoseTest {
 
     @Test fun foo() {
-        val items = arrayOf<Item>(Item("foo", 0, 0))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+       
 
     }
 

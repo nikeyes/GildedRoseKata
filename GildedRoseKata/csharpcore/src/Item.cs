22a81f1739e9f9f25776753704f808dc17dc69f0
FUSION\jorge.castro <jorge.castro@habitaclia.com>
2020-02-27 22:10:34 +0100

    Add ToString .netCore

--- GildedRoseKata/csharpcore/src/Item.cs
+++ GildedRoseKata/csharpcore/src/Item.cs
@@ -5,5 +5,11 @@
         public string Name { get; set; }
         public int SellIn { get; set; }
         public int Quality { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name + ", " + this.SellIn + ", " + this.Quality;
+        }
     }
+
 }
